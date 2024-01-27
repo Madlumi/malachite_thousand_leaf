961b8fbcbd7c0cb7df4c25a81470411f7ab1f893
madi <madi@madanie.com>
2024-01-27 12:15:06 +0200

    basic monthly calc and code formating

--- src/main/java/com/madanie/malachite_thousand_leaf/MalachiteThousandLeafApplication.java
+++ src/main/java/com/madanie/malachite_thousand_leaf/MalachiteThousandLeafApplication.java
@@ -9,15 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MalachiteThousandLeafApplication {
-
    public static void main(String[] args) {
       String file = "prospects.txt";
       try {
          List<Prospect> p = new CsvToBeanBuilder(new FileReader(file)).withType(Prospect.class).withThrowExceptions(false).build().parse();
          for(Prospect pr : p ){System.out.println(pr.toString());}
       } catch (Exception e) {e.printStackTrace();}
-
       //SpringApplication.run(MalachiteThousandLeafApplication.class, args);
    }
-
 }
--- src/main/java/com/madanie/malachite_thousand_leaf/Prospect.java
+++ src/main/java/com/madanie/malachite_thousand_leaf/Prospect.java
@@ -3,60 +3,58 @@ package com.madanie.malachite_thousand_leaf;
 import com.opencsv.bean.CsvBindByName;
 
 public class Prospect {
-    public Prospect() {}
+   public Prospect(){}
 
-    @CsvBindByName(column = "Customer")
-    private String customer;
+   @CsvBindByName(column = "Customer")
+   private String customer;
+   public void   setCustomer(String customer){this.customer = customer;}
+   public String getCustomer(){return customer;}
+   @CsvBindByName(column = "Total Loan")
+   private double totalLoan;
+   public void   setTotalLoan(double totalLoan){this.totalLoan = totalLoan;}
+   public double getTotalLoan(){return totalLoan;}
+   @CsvBindByName(column = "Interest")
+   private double interest;
+   public void   setInterest(double interest){this.interest = interest;}
+   public double getInterest(){return interest;}
+   @CsvBindByName(column = "Years")
+   private double years;
+   public void   setYears(double years){this.years = years;}
+   public double getYears(){return years;}
 
-    @CsvBindByName(column = "Total Loan")
-    private double totalLoan;
+   private double monthly;
+   public double getMonthly(){
+      monthly=calcMonthlyPayment();
+      return monthly;
+   }
 
-    @CsvBindByName(column = "Interest")
-    private double interest;
 
-    @CsvBindByName(column = "Years")
-    private double years;
+   @Override
+   public String toString(){
+      return String.format("Prospect: %s wants to borrow %.2f € for a period of %.2f years and pay %.2f € each month",
+            getCustomer(), getTotalLoan(), getYears(), getMonthly());
+   }
+   //in case of expansion should be rewrtiten to pass in payments 
+   private double calcMonthlyPayment(){
+      // b = Interest on a monthly basis
+      double b  = getInterest() / 12.0 / 100.0 ;
+      // U = Total loan
+      double U = getTotalLoan();
+      // p = Number of payments
+      int p = (int)(getYears() * 12); 
+      // E = Fixed monthly payment
+      // E=U[b(1 + b)^p]/[(1 + b)^p - 1]
+      double E = U * (b*pow((1 + b),p)) / (pow(( 1 + b),p)   - 1);
+      return E;
+   }
 
-    // Getter and setter for Customer
-    public String getCustomer() {
-        return customer;
-    }
+   //TODO move to own class
+   public static double pow(double base,  int factor){
+      double result = 1;
+      for (int i = 0; i<factor; i++){
+         result *= base;
+      }
+      return result;
+   }
 
-    public void setCustomer(String customer) {
-        this.customer = customer;
-    }
-
-    // Getter and setter for Total Loan
-    public double getTotalLoan() {
-        return totalLoan;
-    }
-
-    public void setTotalLoan(double totalLoan) {
-        this.totalLoan = totalLoan;
-    }
-
-    // Getter and setter for Interest
-    public double getInterest() {
-        return interest;
-    }
-
-    public void setInterest(double interest) {
-        this.interest = interest;
-    }
-
-    // Getter and setter for Years
-    public double getYears() {
-        return years;
-    }
-
-    public void setYears(double years) {
-        this.years = years;
-    }
-
-    // toString method
-    @Override
-    public String toString() {
-        return String.format("Prospect: %s wants to borrow %.2f € for a period of %.2f years and pay %.2f € each month",
-                customer, totalLoan, years, 1.1);
-    }
 }
