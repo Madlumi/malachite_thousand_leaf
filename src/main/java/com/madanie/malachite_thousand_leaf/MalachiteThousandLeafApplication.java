ee12253a8af81bd3a25474b61bafdd1e9c1f3ecb
madi <madi@madanie.com>
2024-01-27 10:21:35 +0200

    base project

---
+++ src/main/java/com/madanie/malachite_thousand_leaf/MalachiteThousandLeafApplication.java
@@ -0,0 +1,13 @@
+package com.madanie.malachite_thousand_leaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalachiteThousandLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalachiteThousandLeafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/madanie/malachite_thousand_leaf/MalachiteThousandLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madanie.malachite_thousand_leaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalachiteThousandLeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
